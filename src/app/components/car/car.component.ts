1af5f33554c6745a3e3a357231cdc7868f27e30c
Özlem MİĞFER <ozlemhayalkurun@gmail.com>
2021-04-01 21:51:48 +0300

    Angular

--- src/app/components/car/car.component.ts
+++ src/app/components/car/car.component.ts
@@ -9,7 +9,7 @@ export class CarComponent implements OnInit {
 
   car1={carId:1,brandName:"Range Rover",colorName:"White",modelYear:2021,dailyPrice:100,description:"Evoque"}
   car2={carId:2,brandName:"Mercedes",colorName:"White",modelYear:2014,dailyPrice:150,description:"c180"}
-  car3={carId:3,brandName:"Range Rover",colorName:"Black",modelYear:2019,dailyPrice:110,description:"Velar"}
+  car3={carId:3,brandName:"Range Rover",colorName:"Black",modelYear:2019,dailyPrice:110,description:"Velar"} 
   car4={carId:4,brandName:"Mercedes",colorName:"Black",modelYear:2020,dailyPrice:120,description:"g63"}
   car5={carId:5,brandName:"Range Rover",colorName:"White",modelYear:2018,dailyPrice:80,description:"Sport"}
   car6={carId:6,brandName:"Range Rover",colorName:"White",modelYear:2016,dailyPrice:60,description:"Vogue"}  
